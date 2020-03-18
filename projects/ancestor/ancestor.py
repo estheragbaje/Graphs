367bcdd658a76c32d22370fcb46da80ab15a2d08
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-03-18 19:54:47 +0100

    chore: updated ancestors solution

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -26,15 +26,35 @@ class Graph:
     def __init__(self):
         self.vertices = {}
     
-    def add_vertex(self, vertex_id):
-        if vertex_id not in self.vertices:
-            self.vertices[vertex_id] = set()
+    # def add_vertex(self, vertex_id):
+    #     if vertex_id not in self.vertices:
+    #         self.vertices[vertex_id] = set()
     
+    # def add_edge(self, v1, v2):
+    #     if v1 in self.vertices and v2 in self.vertices:
+    #         self.vertices[v1].add(v2)
+    #     else:
+    #         raise KeyError("That vertex does not exist!")
+
+    def add_vertex(self, vertex_id):
+        """
+        Add a vertex to the graph.
+        """
+        if vertex_id not in self.vertices:
+          return
+        self.vertices[vertex_id] = set()
+
     def add_edge(self, v1, v2):
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].add(v2)
+        """
+        Add a directed edge to the graph.
+        """
+        # check that v1 and v2 exist in the vertices dictionary
+        if v2 not in self.vertices[v1] and v2 in self.vertices and v1 in self.vertices:
+          # add v2 to the vertices at v1
+          self.vertices[v1].add(v2)
         else:
-            raise KeyError("That vertex does not exist!")
+          # raise an exception and give an error
+          raise IndexError("That vertex does not exist")
 
 def earliest_ancestor(ancestors, starting_node):
     # instantiate a new graph object
