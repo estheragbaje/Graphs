0316d19e732b7b9dec044aeb4a9a028d0e31eb16
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-03-18 19:58:06 +0100

    chore: added graph methods to ancestors

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,41 @@
+"""
+Simple graph implementation
+"""
+from util import Stack, Queue  # These may come in handy
+
+class Graph:
+
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        """
+        Add a vertex to the graph.
+        """
+        if vertex_id in self.vertices:
+          return
+        self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        # check that v1 and v2 exist in the vertices dictionary
+        if v2 not in self.vertices[v1] and v2 in self.vertices and v1 in self.vertices:
+          # add v2 to the vertices at v1
+          self.vertices[v1].add(v2)
+        else:
+          # raise an exception and give an error
+          raise IndexError("That vertex does not exist")
+
+    def get_neighbors(self, vertex_id):
+        """
+        Get all neighbors (edges) of a vertex.
+        """
+        return self.vertices[vertex_id]
+
+    def bft(self, starting_vertex):
 
 def earliest_ancestor(ancestors, starting_node):
     pass
